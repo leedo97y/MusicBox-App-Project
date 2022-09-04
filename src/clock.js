143d19efb7ad06473c3c5eb50d045ed9dc5c0a70
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-09-04 19:51:46 +0900

    add date part

--- src/clock.js
+++ src/clock.js
@@ -1,4 +1,32 @@
 const clockBody = document.querySelector("h1");
+const dateBody = document.querySelector("#date");
+
+function date() {
+  const today = new Date();
+
+  const arrMonth = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const arrDay = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+
+  const month = arrMonth[today.getMonth()];
+  const date = JSON.stringify(today.getDate()).padStart(2, "0");
+  const day = arrDay[today.getDay()];
+
+  dateBody.innerText = `${month} ${date} ${day}`;
+}
 
 function clock() {
   const today = new Date();
@@ -9,5 +37,6 @@ function clock() {
   clockBody.innerText = `${hour} : ${minutes} : ${seconds}`;
 }
 
+date();
 clock();
 setInterval(clock, 1000);
