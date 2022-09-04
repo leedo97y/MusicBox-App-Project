a85cb1dc6c4c959df43f22c709fe681719eb72c7
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-09-04 19:33:30 +0900

    add comment on html file, add clock script code

--- src/clock.js
+++ src/clock.js
@@ -1,1 +1,13 @@
+const clockBody = document.querySelector("h1");
 
+function clock() {
+  const today = new Date();
+
+  const hour = JSON.stringify(today.getHours()).padStart(2, "0");
+  const minutes = JSON.stringify(today.getMinutes()).padStart(2, "0");
+  const seconds = JSON.stringify(today.getSeconds()).padStart(2, "0");
+  clockBody.innerText = `${hour} : ${minutes} : ${seconds}`;
+}
+
+clock();
+setInterval(clock, 1000);
