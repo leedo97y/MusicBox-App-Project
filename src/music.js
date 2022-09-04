e3e204a8bdd6905d0629479ebbc911c9bdd8b12a
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-09-05 02:02:28 +0900

    add music script code

---
+++ src/music.js
@@ -0,0 +1,47 @@
+// music player body
+const musicPlayer = document.querySelector("#container");
+
+// music titles
+const title = document.querySelector("#music-title");
+const detailTitle = document.querySelector("#music-detail-title");
+
+// music player button
+const previous = document.querySelector("#previous");
+const play = document.querySelector("#play");
+const pause = document.querySelector("#pause");
+const next = document.querySelector("#next");
+
+// volume
+const volume = document.querySelector("#volume-range");
+
+const audioList = [
+  "audio/Cafe Regrette - Asher Fulero.mp3",
+  "audio/King Porter Stomp - Joel Cummins.mp3",
+  "audio/Modern Attempt - TrackTribe.mp3",
+  "audio/Resolve - Joel Cummins.mp3",
+  "audio/Smokey's Lounge - TrackTribe.mp3",
+  "audio/The Joint is Jumpin - Joel Cummins.mp3",
+];
+
+const audio = new Audio();
+console.log();
+
+function goPrevious() {
+  //
+}
+
+function goNext() {
+  //
+}
+
+function onPlay() {
+  // const current =
+}
+
+function onPause() {
+  //
+}
+
+function onChange() {
+  //
+}
