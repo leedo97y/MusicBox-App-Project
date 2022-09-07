51f5654f4ac9386e5ce8923e0f26d3fcc4ff5542
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-09-07 22:08:19 +0900

    add weather component

---
+++ src/weather.js
@@ -0,0 +1,29 @@
+const API_KEY = "c085c85ef7adac74dca0cca6afe4cb80";
+
+const icon = document.querySelector("#weather-icon");
+const degree = document.querySelector("#degree");
+const city = document.querySelector("#city");
+const rain = document.querySelector("#rain");
+
+function getGeo(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=Metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const iconStr = data.weather[0].icon;
+      const ICON_URL = `http://openweathermap.org/img/wn/${iconStr}@2x.png`;
+      icon.innerHTML = `<img src="${ICON_URL}" />`;
+      degree.innerText = `${Math.floor(data.main.temp)}°`;
+      city.innerText = data.name;
+      rain.innerText = `${data.clouds.all}%`;
+    });
+}
+
+function getGeoError() {
+  alert("error!");
+}
+
+navigator.geolocation.getCurrentPosition(getGeo, getGeoError);
